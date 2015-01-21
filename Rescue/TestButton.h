b525283c3ecb664b7ce2b23be6841f121c503f7c
Alex Chen <alexc@bsquare.com>
2015-01-22 00:14:08 +0800

    add amazing button

---
+++ Rescue/TestButton.h
@@ -0,0 +1,13 @@
+//
+//  TestButton.h
+//  Rescue
+//
+//  Created by Alex Chen on 2015/1/21.
+//  Copyright (c) 2015年 Alex Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestButton : NSObject
+
+@end
